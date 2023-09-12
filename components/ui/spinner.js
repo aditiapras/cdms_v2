31677bc5296475a05f7e8ba2a8784dbcba79439d
Aditia Prasetian <aditiapras97@gmail.com>
2023-09-12 23:17:28 +0700

    Create spinner.js

---
+++ components/ui/spinner.js
@@ -0,0 +1,12 @@
+import { BiLoaderCircle } from "react-icons/bi";
+
+export default function Spinner() {
+  return (
+    <div className="absolute top-0 left-0 w-full min-h-screen flex items-center justify-center bg-white">
+      <div className="flex items-center justify-center gap-2 text-xl">
+        <BiLoaderCircle className="animate-spin"></BiLoaderCircle>
+        <p>Loading...</p>
+      </div>
+    </div>
+  );
+}
