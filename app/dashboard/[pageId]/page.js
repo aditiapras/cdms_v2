95999eec3c097a9af778eaa543c866ba9bc891d3
Aditia Prasetian <aditiapras97@gmail.com>
2023-09-12 23:17:14 +0700

    Update page.js

--- app/dashboard/[pageId]/page.js
+++ app/dashboard/[pageId]/page.js
@@ -2,14 +2,25 @@ import React from "react";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import Monitoring from "@/components/monitoring/monitoring";
+import InputNaik from "@/components/input/input";
 
 export default async function DynamicDash({ params }) {
   const session = await getServerSession(authOptions);
-  console.log(session);
 
   if (!session) {
     return redirect("/");
   }
 
-  return <div>{params.pageId}</div>;
+  return (
+    <div className="h-full">
+      {params.pageId == "monitoring" ? (
+        <Monitoring />
+      ) : params.pageId == "input" ? (
+        <InputNaik />
+      ) : (
+        params.pageId
+      )}
+    </div>
+  );
 }
