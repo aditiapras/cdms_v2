c575ee4296352f107e3cfddf4ab940d3e7437ec9
Aditia Prasetian <aditiapras97@gmail.com>
2023-09-12 11:33:41 +0700

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,5 +1,13 @@
+import Logout from "@/components/dashboard/logout";
+import DashboardSession from "@/components/dashboard/session";
 import React from "react";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      Dashboard
+      <DashboardSession />
+      <Logout />
+    </div>
+  );
 }
