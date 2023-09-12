b3cbd506a66f2a8d3a61b7e25d0639e8fef6cafe
Aditia Prasetian <aditiapras97@gmail.com>
2023-09-12 23:17:18 +0700

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,13 +1,18 @@
-import Logout from "@/components/dashboard/logout";
-import DashboardSession from "@/components/dashboard/session";
-import React from "react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const session = await getServerSession(authOptions);
+
   return (
-    <div>
-      Dashboard
-      <DashboardSession />
-      <Logout />
+    <div className="flex flex-col items-center justify-center h-full bg-zinc-100">
+      <p className="text-4xl">
+        Welcome back,{" "}
+        <span className="font-bold">{session?.user?.username}</span>
+      </p>
+      <p className="mt-20 text-xs text-zinc-400">
+        Copyright © 2023 Hankook Tire Indonesia
+      </p>
     </div>
   );
 }
