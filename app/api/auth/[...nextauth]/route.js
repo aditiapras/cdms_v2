9a8fd4950a32d30a9b4d5b667212c3b1b76fc29f
Aditia Prasetian <aditiapras97@gmail.com>
2023-09-12 11:33:37 +0700

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,44 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
+import axios from "axios";
 
 export const authOptions = {
   providers: [
     CredentialsProvider({
       name: "credentials",
       credentials: {},
-      async authorize(credentials) {},
+      async authorize(credentials) {
+        try {
+          const { nik, password } = credentials;
+          const res = await axios.post(
+            `${process.env.NEXTAUTH_URL}/api/login`,
+            {
+              nik,
+              password,
+            }
+          );
+          const user = res.data;
+          const matchPassword = await bcrypt.compare(password, user.password);
+
+          if (!user) {
+            return null;
+          }
+
+          if (!matchPassword) {
+            return null;
+          }
+
+          console.log(user);
+          return {
+            nik: user.nik,
+            usename: user.username,
+            workgroup: user.workgroup,
+          };
+        } catch (error) {
+          return null;
+        }
+      },
     }),
   ],
   callbacks: {
