60e75452939ae824c17506bf0fef12a23f25c70f
Aditia Prasetian <aditiapras97@gmail.com>
2023-09-12 23:17:20 +0700

    Update logout.js

--- components/dashboard/logout.js
+++ components/dashboard/logout.js
@@ -1,12 +1,16 @@
 "use client";
 import React from "react";
 import { signOut } from "next-auth/react";
+import { FaSignOutAlt } from "react-icons/fa";
 
 export default function Logout() {
   return (
     <div>
-      <button onClick={() => signOut()} className="border p-2">
-        Sign out
+      <button
+        onClick={() => signOut()}
+        className="flex items-center bg-zinc-200 rounded-md p-2 hover:bg-zinc-300"
+      >
+        <FaSignOutAlt className="text-4xl text-zinc-400"></FaSignOutAlt>
       </button>
     </div>
   );
