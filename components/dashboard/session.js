fb09dcfd4ab701036b353ffd43cbcd30ff161e28
Aditia Prasetian <aditiapras97@gmail.com>
2023-09-12 23:17:22 +0700

    Update session.js

--- components/dashboard/session.js
+++ components/dashboard/session.js
@@ -1,9 +1,71 @@
 "use client";
-import React from "react";
-import { useSession } from "next-auth/react";
 
-export default function DashboardSession() {
+import { useSession } from "next-auth/react";
+import { BiSolidDashboard } from "react-icons/bi";
+import { TbHeartRateMonitor } from "react-icons/tb";
+import { AiOutlineClear } from "react-icons/ai";
+import { GiOilDrum } from "react-icons/gi";
+import { BsFileEarmarkPlusFill } from "react-icons/bs";
+import { FaHistory } from "react-icons/fa";
+import Link from "next/link";
+import Logout from "./logout";
+
+export default function DashboardSession({ components }) {
   const { data: session } = useSession();
-  console.log(session);
-  return <div>DashboardSession</div>;
+
+  return (
+    <main className="flex w-full min-h-screen">
+      <div className="flex flex-col gap-5 w-20 border-r p-4 max-h-screen sticky top-0 z-10">
+        <div className="flex flex-col h-full gap-5 items-center">
+          <Link
+            href={"/dashboard"}
+            className="flex items-center bg-zinc-200 rounded-md p-2 hover:bg-zinc-300"
+          >
+            <BiSolidDashboard className="text-4xl text-zinc-400"></BiSolidDashboard>
+          </Link>
+          <Link
+            href={"/dashboard/input"}
+            className="flex items-center bg-zinc-200 rounded-md p-2 hover:bg-zinc-300"
+          >
+            <BsFileEarmarkPlusFill className="text-4xl text-zinc-400"></BsFileEarmarkPlusFill>
+          </Link>
+          <Link
+            href={"/dashboard/monitoring"}
+            className="flex items-center bg-zinc-200 rounded-md p-2 hover:bg-zinc-300"
+          >
+            <TbHeartRateMonitor className="text-4xl text-zinc-400"></TbHeartRateMonitor>
+          </Link>
+          <Link
+            href={"/dashboard/drums"}
+            className="flex items-center bg-zinc-200 rounded-md p-2 hover:bg-zinc-300"
+          >
+            <GiOilDrum className="text-4xl text-zinc-400"></GiOilDrum>
+          </Link>
+          <Link
+            href={"/dashboard/historycleaning"}
+            className="flex items-center bg-zinc-200 rounded-md p-2 hover:bg-zinc-300"
+          >
+            <FaHistory className="text-4xl text-zinc-400"></FaHistory>
+          </Link>
+          <Link
+            href={"/dashboard/clean-carcass"}
+            className="flex items-center bg-zinc-200 rounded-md p-2 hover:bg-zinc-300"
+          >
+            <AiOutlineClear className="text-4xl text-zinc-400"></AiOutlineClear>
+          </Link>
+        </div>
+        <Logout />
+      </div>
+      <div className="flex w-full flex-col">
+        <div className="w-full h-16 shadow-md px-5 flex items-center justify-between sticky top-0 bg-white/60 backdrop-blur-md z-10">
+          <p className="text-2xl font-semibold">Dashboard</p>
+          <p className="text-xs bg-green-100 text-green-700 p-1 rounded-md">
+            Logged in as:{" "}
+            <span className="font-semibold">{session?.user?.username}</span>
+          </p>
+        </div>
+        <div className="w-full flex flex-col h-full">{components}</div>
+      </div>
+    </main>
+  );
 }
