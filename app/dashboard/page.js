4b4fa7868c1a3a2ce2fd8e32d577fb9471fd53a6
Aditia Prasetian <aditiapras97@gmail.com>
2023-09-13 21:28:45 +0700

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,8 +1,13 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
 export default async function Dashboard() {
   const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return redirect("/");
+  }
 
   return (
     <div className="flex flex-col items-center justify-center h-full bg-zinc-100">
