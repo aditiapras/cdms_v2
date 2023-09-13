888cb101821f5aee3a048f04d5f50fafb689f103
Aditia Prasetian <aditiapras97@gmail.com>
2023-09-13 23:21:01 +0700

    Update drum.js

--- components/monitoring/drum.js
+++ components/monitoring/drum.js
@@ -2,8 +2,19 @@ import Link from "next/link";
 import { useFetch } from "@/lib/fetchHelper";
 import moment from "moment-timezone";
 import Spinner from "../ui/spinner";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
-export default function CarcassDrum({ drum }) {
+export default function CarcassDrum({ drum, mesin }) {
   const { data, error, isLoading } = useFetch(
     `http://localhost:3000/api/cdms/drums?id=${drum}`
   );
@@ -21,19 +32,38 @@ export default function CarcassDrum({ drum }) {
           No C/C Drum Mounted
         </p>
       ) : (
-        <Link
-          href={`/dashboard/monitoring/${drum}`}
-          className={`${
-            age < 10
-              ? "bg-green-200 hover:bg-green-300"
-              : age >= 10 && age <= 14
-              ? "bg-amber-200 hover:bg-amber-300"
-              : "bg-red-200 hover:bg-red-300"
-          } flex items-center justify-between border border-zinc-300 rounded-lg px-5 md:px-2 h-12 transition duration-200 text-sm`}
-        >
-          <p>{drum}</p>
-          <p>{age}</p>
-        </Link>
+        <AlertDialog>
+          <AlertDialogTrigger
+            className={`${
+              age < 10
+                ? "bg-green-200 hover:bg-green-300"
+                : age >= 10 && age <= 14
+                ? "bg-amber-200 hover:bg-amber-300"
+                : "bg-red-200 hover:bg-red-300"
+            } flex items-center justify-between border border-zinc-300 rounded-lg px-5 md:px-2 h-12 transition duration-200 text-sm`}
+          >
+            <p>{drum}</p>
+            <p>{age}</p>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Apakah anda yakin?</AlertDialogTitle>
+              <AlertDialogDescription>
+                Proses ini akan menurunkan C/C Drum{" "}
+                <span className="font-bold text-zinc-950">{drum}</span> dari
+                mesin <span className="font-bold text-zinc-950">{mesin}</span>
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction asChild>
+                <Link href={`/dashboard/monitoring/${drum}`}>
+                  <p>Continue</p>
+                </Link>
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       )}
     </>
   );
