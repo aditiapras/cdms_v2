090a31fe093a361106eece2d491b29a1b22db190
aditiaprasdev <aditiapras97@gmail.com>
2023-09-15 16:11:56 +0700

    Update eachDrum.js

--- components/ccdrum/eachDrum.js
+++ components/ccdrum/eachDrum.js
@@ -23,9 +23,9 @@ export default function EachDrum({ id_drum }) {
       : data.age;
 
   return (
-    <HoverCard openDelay={200} closeDelay={0}>
-      <HoverCardTrigger asChild>
-        <button
+    <>
+      {data.status == "unuse" ? (
+        <div
           className={`${
             age > 0 && age < 10
               ? "bg-green-200 hover:bg-green-300"
@@ -38,32 +38,51 @@ export default function EachDrum({ id_drum }) {
         >
           <p className="">{data.id_drum}</p>
           <p className="">{age}</p>
-        </button>
-      </HoverCardTrigger>
-      <HoverCardContent className="w-72" align="start">
-        <div className="flex justify-between space-x-4">
-          <div className="space-y-1">
-            <h4 className="text-sm font-semibold">{data.id_drum}</h4>
-            <p className="text-xs">
-              Mesin Building:{" "}
-              <span className="font-semibold">{data.building_mc}</span>
-            </p>
-            <p className="text-xs">
-              Umur Pakai: <span className="font-semibold">{age} Hari</span>
-            </p>
-            <div className="flex items-center pt-2">
-              {age == 0 ? (
-                ""
-              ) : (
-                <span className="text-xs text-muted-foreground">
-                  Tanggal Naik:{" "}
-                  {moment(data.date_naik).format("DD MMMM YYYY HH:mm")}
-                </span>
-              )}
-            </div>
-          </div>
         </div>
-      </HoverCardContent>
-    </HoverCard>
+      ) : (
+        <HoverCard openDelay={200} closeDelay={0}>
+          <HoverCardTrigger asChild>
+            <button
+              className={`${
+                age > 0 && age < 10
+                  ? "bg-green-200 hover:bg-green-300"
+                  : age >= 10 && age <= 14
+                  ? "bg-amber-200 hover:bg-amber-300"
+                  : age > 14
+                  ? "bg-red-200 hover:bg-red-300"
+                  : "bg-white hover:bg-zinc-100"
+              } flex justify-between h-12 items-center p-2 border rounded-md text-xs transition duration-200`}
+            >
+              <p className="">{data.id_drum}</p>
+              <p className="">{age}</p>
+            </button>
+          </HoverCardTrigger>
+          <HoverCardContent className="w-72" align="start">
+            <div className="flex justify-between space-x-4">
+              <div className="space-y-1">
+                <h4 className="text-sm font-semibold">{data.id_drum}</h4>
+                <p className="text-xs">
+                  Mesin Building:{" "}
+                  <span className="font-semibold">{data.building_mc}</span>
+                </p>
+                <p className="text-xs">
+                  Umur Pakai: <span className="font-semibold">{age} Hari</span>
+                </p>
+                <div className="flex items-center pt-2">
+                  {age == 0 ? (
+                    ""
+                  ) : (
+                    <span className="text-xs text-muted-foreground">
+                      Tanggal Naik:{" "}
+                      {moment(data.date_naik).format("DD MMMM YYYY HH:mm")}
+                    </span>
+                  )}
+                </div>
+              </div>
+            </div>
+          </HoverCardContent>
+        </HoverCard>
+      )}
+    </>
   );
 }
