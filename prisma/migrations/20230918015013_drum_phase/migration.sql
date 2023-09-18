f805f3d223b7bfeacc46d70b05b7be6369eaf1ff
aditiaprasdev <aditiapras97@gmail.com>
2023-09-18 16:11:51 +0700

    Create migration.sql

---
+++ prisma/migrations/20230918015013_drum_phase/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `date_cleaning` on the `cleaning` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+  - You are about to alter the column `date_naik` on the `drum` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+  - You are about to alter the column `date_turun` on the `drum` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+  - Added the required column `phase` to the `Drum` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `cleaning` MODIFY `date_cleaning` DATETIME NOT NULL;
+
+-- AlterTable
+ALTER TABLE `drum` ADD COLUMN `phase` VARCHAR(10) NOT NULL,
+    MODIFY `date_naik` DATETIME NULL,
+    MODIFY `date_turun` DATETIME NULL;
