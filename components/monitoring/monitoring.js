664a56ca9526cf007bb629d57f5b5d3440005c21
aditiaprasdev <aditiapras97@gmail.com>
2023-09-18 16:11:33 +0700

    Update monitoring.js

--- components/monitoring/monitoring.js
+++ components/monitoring/monitoring.js
@@ -2,34 +2,44 @@
 import CarcassDrum from "./drum";
 import { useFetch } from "@/lib/fetchHelper";
 import Spinner from "../ui/spinner";
+import { useState } from "react";
 
 export default function Monitoring() {
   const { data, error, isLoading } = useFetch(
-    `http://localhost:3000/api/cdms/monitoring`
+    `${process.env.NEXT_PUBLIC_URL}/api/cdms/monitoring`
   );
+
+  const [phase, setPhase] = useState("Phase 1");
 
   if (error) return <div>failed to load</div>;
   if (isLoading) return <Spinner />;
 
   return (
     <main className="flex flex-col h-full">
-      <div className="flex justify-between w-full p-5 border-b bg-zinc-50">
+      <div className="flex items-center gap-5 w-full p-5 border-b bg-zinc-50">
         <p className="font-semibold">Monitoring</p>
-        <select name="phase" id="phase" className="bg-zinc-50">
+        <select
+          name="phase"
+          id="phase"
+          className="bg-zinc-100 hover:bg-zinc-200 p-2 rounded-md transition duration-200"
+          onChange={(e) => setPhase(e.target.value)}
+        >
           <option value="Phase 1">Phase 1</option>
           <option value="Phase 2">Phase 2</option>
         </select>
       </div>
       <div className="grid grid-cols-5 gap-5 w-full p-5 bg-zinc-100 h-full">
-        {data.map((drum) => (
-          <div key={drum.building_mc} className="flex flex-col gap-2">
-            <p className="font-semibold">{drum.building_mc}</p>
-            <div className="flex flex-col gap-0.5">
-              <CarcassDrum drum={drum.id_left} mesin={drum.building_mc} />
-              <CarcassDrum drum={drum.id_right} mesin={drum.building_mc} />
+        {data
+          .filter((drum) => drum.phase == phase)
+          .map((drum) => (
+            <div key={drum.building_mc} className="flex flex-col gap-2">
+              <p className="font-semibold">{drum.building_mc}</p>
+              <div className="flex flex-col gap-0.5">
+                <CarcassDrum drum={drum.id_left} mesin={drum.building_mc} />
+                <CarcassDrum drum={drum.id_right} mesin={drum.building_mc} />
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </main>
   );
