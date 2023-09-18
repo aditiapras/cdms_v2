deebd7a471e3721cc492bcf77d0a75df5d52f33d
aditiaprasdev <aditiapras97@gmail.com>
2023-09-18 16:11:44 +0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -6,7 +6,10 @@ export function middleware() {
 
   // add the CORS headers to the response
   res.headers.append("Access-Control-Allow-Credentials", "true");
-  res.headers.append("Access-Control-Allow-Origin", "http://192.168.187.120"); // replace this your actual origin
+  res.headers.append(
+    "Access-Control-Allow-Origin",
+    `${process.env.NEXTAUTH_URL}`
+  ); // replace this your actual origin
   res.headers.append(
     "Access-Control-Allow-Methods",
     "GET,DELETE,PATCH,POST,PUT"
