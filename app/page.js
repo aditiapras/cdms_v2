73e49ecb5a30e9ea84d4cdc5895ac94864f0a007
aditiaprasdev <aditiapras97@gmail.com>
2023-09-21 15:17:08 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,13 @@
 import Login from "@/components/login";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  if (session) {
+    return redirect("/dashboard");
+  }
   return (
     <main>
       <Login />
