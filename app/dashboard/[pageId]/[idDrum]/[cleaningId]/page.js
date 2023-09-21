b867269a7fdb232132fc9791b557c47d78d3e250
aditiaprasdev <aditiapras97@gmail.com>
2023-09-21 15:17:03 +0700

    Update page.js

--- app/dashboard/[pageId]/[idDrum]/[cleaningId]/page.js
+++ app/dashboard/[pageId]/[idDrum]/[cleaningId]/page.js
@@ -8,8 +8,10 @@ import { useSession } from "next-auth/react";
 import { useFetch } from "@/lib/fetchHelper";
 import { redirect } from "next/navigation";
 import { cleanDrumMonitoring, cleanDrums } from "@/lib/cleanHelper";
+import { useState } from "react";
 
 export default function CleaningCC({ params }) {
+  const [qty, setQty] = useState(0);
   const { register, handleSubmit, control, reset } = useForm();
   const { fields } = useFieldArray({
     control,
@@ -69,17 +71,23 @@ export default function CleaningCC({ params }) {
 
   return (
     <div className="w-full h-full bg-zinc-100 flex flex-col gap-5 p-5 items-center justify-center">
-      <p className="text-xl font-semibold">Cleaning Carcass Drum</p>
-      <div className="flex justify-between gap-5">
+      <div className="flex justify-between gap-5 bg-white p-5 rounded-md shadow-md">
         <form
           onSubmit={handleSubmit(onSubmit)}
           className="flex flex-col w-full p-3 gap-3 text-sm"
         >
+          <Link
+            href="/dashboard/drums"
+            className="bg-red-400 hover:bg-red-300 text-white p-2 rounded-md w-[80px] text-center transition duration-200"
+          >
+            Cancel
+          </Link>
+          <p className="text-xl font-semibold">Cleaning Carcass Drum</p>
           <div className="flex gap-3">
             <p>ID Drum:</p>
             <input
               type="text"
-              className="bg-zinc-100 focus-visible:outline-none font-semibold"
+              className="bg-white focus-visible:outline-none font-semibold"
               {...register("id_drum")}
               readOnly
               value={params.cleaningId}
@@ -105,12 +113,12 @@ export default function CleaningCC({ params }) {
               <input
                 type="text"
                 value={part.name}
-                className="bg-zinc-100 focus-visible:outline-none"
+                className="bg-white focus-visible:outline-none"
                 {...register(`parts.${index}.name`)}
                 readOnly
               />
               <select
-                className="p-2 rounded-md"
+                className="p-2 rounded-md border"
                 {...register(`parts.${index}.status`)}
                 defaultValue={"OK"}
               >
@@ -118,7 +126,7 @@ export default function CleaningCC({ params }) {
                 <option value="NG">NG</option>
               </select>
               <select
-                className="p-2 rounded-md"
+                className="p-2 rounded-md border"
                 {...register(`parts.${index}.change`)}
                 defaultValue={"No"}
               >
@@ -128,26 +136,18 @@ export default function CleaningCC({ params }) {
               <input
                 type="number"
                 defaultValue={0}
-                className="p-2 rounded-md w-14"
+                className="p-2 rounded-md w-14 border"
                 {...register(`parts.${index}.qty`)}
               />
             </div>
           ))}
-          <div className="flex justify-end text-center w-full">
-            <div className="flex w-1/3 gap-5">
-              <Link
-                href="/dashboard/drums"
-                className="bg-zinc-200 hover:bg-zinc-50 p-2 rounded-md w-full transition duration-200"
-              >
-                Cancel
-              </Link>
-              <button
-                type="submit"
-                className="bg-zinc-950 text-white hover:bg-zinc-700 p-2 rounded-md w-full transition duration-200"
-              >
-                Submit
-              </button>
-            </div>
+          <div className="flex text-center w-full mt-5">
+            <button
+              type="submit"
+              className="bg-zinc-950 text-white hover:bg-zinc-700 p-2 rounded-md w-full transition duration-200"
+            >
+              Submit
+            </button>
           </div>
         </form>
       </div>
