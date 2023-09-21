30119f6572073829c3fd3b893f09dc0503c62181
aditiaprasdev <aditiapras97@gmail.com>
2023-09-21 15:17:10 +0700

    Update page.js

--- app/register/page.js
+++ app/register/page.js
@@ -1,7 +1,13 @@
-import React from "react";
 import CreateAccount from "./register";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-export default function Register() {
+export default async function Register() {
+  const session = await getServerSession(authOptions);
+  if (session) {
+    return redirect("/dashboard");
+  }
   return (
     <div>
       <CreateAccount />
