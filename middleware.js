c4ebf5a98df372ad7711dc48cd756d55e49c4a2a
aditiaprasdev <aditiapras97@gmail.com>
2023-10-06 10:31:25 +0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,25 +1,3 @@
-import { NextResponse } from "next/server";
-
-// export function middleware() {
-//   // retrieve the current response
-//   const res = NextResponse.next();
-
-//   // add the CORS headers to the response
-//   res.headers.append("Access-Control-Allow-Credentials", "true");
-//   res.headers.append("Access-Control-Allow-Origin", `${process.env.CORS}`); // replace this your actual origin
-//   res.headers.append(
-//     "Access-Control-Allow-Methods",
-//     "GET,DELETE,PATCH,POST,PUT"
-//   );
-//   res.headers.append(
-//     "Access-Control-Allow-Headers",
-//     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-//   );
-
-//   return res;
-// }
-
 export { default } from "next-auth/middleware";
 
-// export const config = { matcher: ["/dashboard", "/api/:path*"] };
 export const config = { matcher: ["/dashboard"] };
